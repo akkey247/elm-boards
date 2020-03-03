4d5d07841e76e51f7aed0a93d0c88ae1178dc810
akkey <akkey52417@gmail.com>
2020-03-03 22:03:09 +0900

    Laravelで作ったAPIを追加

---
+++ source/api/app/Http/Middleware/Cors.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Support\Facades\Log;
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {   
+        $response = $next($request);
+
+        $http_origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : "";
+
+        Log::debug("http_origin = " . $http_origin);
+        if ($http_origin == "http://localhost:8080") {
+            $response
+                ->header("Access-Control-Allow-Origin" , "http://localhost:8080")
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+        }
+
+        return $response;
+    }
+}
